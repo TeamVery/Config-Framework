3b917910b071dc73f1b1a9496851abe883f7b4ba
Kill00 <hellow1211other@gmail.com>
2021-01-19 18:50:14 +0900

    Initial commit

---
+++ src/main/java/com/teamvery/configframework/Conf.java
@@ -0,0 +1,67 @@
+package com.teamvery.configframework;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Conf {
+
+    private static File file;
+    public static FileConfiguration config;
+
+    public static void makeData(String Plugin_Name, String yml) {
+        try {
+            file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin(Plugin_Name)).getDataFolder(), yml);
+            if (!file.exists()) {
+                Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin(Plugin_Name)).saveResource(yml, true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    @Deprecated
+    public static void setup(String Plugin_Name, String yml) {
+        file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin(Plugin_Name)).getDataFolder(), yml);
+
+        if (!file.exists()){
+            try{
+                file.createNewFile();
+            }catch (IOException e){
+                System.out.println("알수 없는 오류로 콘피그 파일을 생성할수 없습니다.");
+            }Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin(Plugin_Name)).saveResource(yml, true);
+        }
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public static FileConfiguration get(String Plugin_Name, String yml) {
+        file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin(Plugin_Name)).getDataFolder(), yml);
+
+        config = YamlConfiguration.loadConfiguration(file);
+        return config;
+    }
+
+    @Deprecated
+    public static void save(String Plugin_Name, String yml) {
+        file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin(Plugin_Name)).getDataFolder(), yml);
+
+        config = YamlConfiguration.loadConfiguration(file);
+
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void reload(String Plugin_Name, String yml){
+        file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin(Plugin_Name)).getDataFolder(), yml);
+
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+}
---
+++ src/main/java/com/teamvery/configframework/ConfigFramework.java
@@ -0,0 +1,11 @@
+package com.teamvery.configframework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ConfigFramework extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabled!");
+    }
+}
